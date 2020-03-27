c5b5b81d014999dc7109e7525a68c15bfc3a592b
jinam <jinam@DESKTOP-HVOAHE5>
2020-03-27 15:11:04 +0900

    버블 정렬

--- kakao1/src/listing/Main2.java
+++ kakao1/src/listing/Main2.java
@@ -11,8 +11,8 @@ public class Main2 {
 
 		print(sort);
 		System.out.println();
-
-		for (int i = 0; i < sort.length; i++) {//첫번째 비교대상
+		//여기서 i가 증가하면서 하는 역할은 sort.length-i로 활용하고자 하는것
+		for (int i = 0; i < sort.length; i++) {
 			for (int j = 0; j < sort.length - 1; j++) {// 어디까지 비교할지 비교대상//버블정렬이라 처음엔 0~9까지 그다음엔 0~8까지 이런식으로 뒤쪽이 줄어들기 때문
 				if (sort[j] > sort[j + 1]) {
 					temp = sort[j]; // 안쪽 for문에서 구한 최소값을 temp에 넣음
